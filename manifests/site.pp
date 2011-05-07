b08ad673645ebeaff66879e3d2706ae9989d740a
Kris Buytaert <sdog@inuits.be>
2011-05-07 14:20:06 +0200

    initial nonworking setup

---
+++ manifests/site.pp
@@ -0,0 +1,70 @@
+class repo {
+    $releasever = "5"
+    $basearch = "i386"
+
+
+    yumrepo {
+
+
+	
+	"percona":
+	    descr 	=> "Percona", 
+	    enabled 	=>1, 
+	    baseurl 	=> "http://repo.percona.com/centos/$releasever/os/$basearch/",
+	    gpgcheck 	=> 0;
+
+
+	"ourdelta":
+	    descr 	=> "Ourdelta",
+	    enabled 	=> 1,
+	    gpgcheck 	=> 0,
+	    baseurl 	=> "http://master.ourdelta.org/yum/CentOS-MySQL50/5Server/"; 
+        
+        "epel":
+            descr 	=> "Epel-5",
+            baseurl 	=> "http://mirror.eurid.eu/epel/5/i386/",
+            enabled 	=> 1,
+            gpgcheck 	=> 0;
+        }
+
+
+}
+
+class demo-mysql-databases {
+
+ mysql::rights{"Set rights for puppet database":
+  ensure   => present,
+  database => "puppet",
+  user     => "puppet",
+  password => "puppet"
+ }
+
+ mysql::database{"mydb":
+  ensure   => present
+ }
+
+ mysql::database{"testeke":
+  ensure   => present
+ }
+
+
+}
+node mysql {
+
+    $hostname = 'MDC-A'
+    $mysql_root_password = 'SARDINES'
+    $mysql_serverid = 1
+    $replicate_ignore_db = 'mysql,puppet_dashboard,puppet_dashboard_test' 
+
+
+    include repo
+    include mysql::server
+    include demo-mysql-databases 
+
+    Class["repo"] ->  Class ["mysql::server"] -> Class["ntc-mysql-databases"]
+    
+
+
+
+
+}
---
+++ modules/mysql/manifests/init.pp
@@ -0,0 +1,82 @@
+# server.pp
+
+
+class mysql::server  {
+
+  include mysql::packages
+  include mysql::config
+  
+  Class['mysql::packages'] -> Class['mysql::config'] 
+  
+}
+
+class mysql::packages {
+
+        package {
+                "MySQL-client-community":
+                        ensure => "installed";
+                "MySQL-server-community":
+                        ensure => "installed";
+                "MySQL-shared-compat":
+                        ensure => "installed";
+        }
+}
+
+
+class mysql::config {
+
+file {
+	"/etc/my.cnf": 
+		owner => "root",
+ 		group => "root",		
+		ensure => file,
+		content => template("mysql/my.cnf.erb"),
+		notify => Service['mysql'];
+        "/var/log/mysql/":
+                ensure => "directory";
+        "/var/lib/mysql/":
+                ensure => "directory";
+        "/var/lib/mysql/.tmp":
+                ensure => "directory",
+                owner => "mysql",
+                group => 'mysql',
+		require => Package['MySQL-client-community'];
+}
+
+
+service {
+                "mysql":
+                        enable  => true,
+			ensure => running,
+                        require => [Package['MySQL-server-community'],File['/var/lib/mysql/.tmp']];
+        }
+
+}
+
+
+class mysql::setrootpw {
+
+  $mysql_user = "root"
+  file { "/root/.my.cnf":
+      owner => root,
+      group => root,
+      mode => 600,
+   require => Exec["Initialize MySQL server root password"]
+    }
+
+
+   exec { "Initialize MySQL server root password":
+                unless  => "test -f /root/.my.cnf",
+                command => "mysqladmin -u${mysql_user} password '${mysql_root_password}'",
+                notify  => Exec["Generate my.cnf"],
+                require =>  Service['mysql'] 
+        }
+
+
+
+  exec { "Generate my.cnf":
+    command => "/bin/echo -e \"[mysql]\nuser=${mysql_user}\npassword=${mysql_root_password}\n[mysqladmin]\nuser=${mysql_user}\npassword=${mysql_root_password}\n[mysqldump]\nuser=${mysql_user}\npassword=${mysql_root_password}\n[mysqlshow]\nuser=${mysql_user}\npassword=${mysql_root_password}\n\" > /root/.my.cnf",
+    refreshonly => true,
+    creates => "/root/.my.cnf",
+  }
+}
