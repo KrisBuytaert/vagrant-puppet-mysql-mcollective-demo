e3315de068ad040da80e90e27d857b429a84fda4
Frédéric Descamps <lefred.descamps@gmail.com>
2011-05-09 14:56:44 +0200

    add the right facter files
    ex:
    
    [root@mc_master ~]# mc-facts kernel
    Report for fact: kernel
    
            Linux                                   found 1 times
    
            Finished processing 1 / 1 hosts in 1857.05 ms
    
    Signed-off-by: Frédéric Descamps <lefred.descamps@gmail.com>

--- modules/mcollective/files/plugins/facts/facter/facter.rb
+++ modules/mcollective/files/plugins/facts/facter/facter.rb
@@ -6,7 +6,7 @@ module MCollective
         #
         # This plugin by default works with puppet facts loaded via pluginsync
         # and the deprecated factsync. If your facts are in a custom location, or
-        # you use non-standard puppet dirs, then set plugin.facter.facterlib 
+        # you use non-standard puppet dirs, then set plugin.facter.facterlib
         # in the server.cfg
         #
         # It caches facts for 300 seconds to speed things up a bit,
@@ -19,9 +19,10 @@ module MCollective
         # Plugin released under the terms of the GPL.
         class Facter<Base
             @@last_facts_load = 0
+            @@facts = {}
+            @@last_good_facts = {}
 
             def get_facts
-
                 config = Config.instance
                 logger = Log.instance
 
@@ -29,18 +30,46 @@ module MCollective
                 cache_time = config.pluginconf["facter.cache_time"] || 300
 
                 logger.debug("Have FACTERLIB: #{ENV['FACTERLIB']}")
+                logger.debug("Last facts load time is #{@@last_facts_load} cache time is #{cache_time}")
 
-                begin 
-                    if (Time.now.to_i - @@last_facts_load > cache_time.to_i )
+                Thread.exclusive do
+                    begin
+                        if (Time.now.to_i - @@last_facts_load > cache_time.to_i )
+                            logger.debug("Resetting facter cache after #{cache_time} seconds")
+                            reload_facts
+                        end
+                    rescue Exception => e
+                        logger.error("Failed to load facts: #{e}")
                         @@last_facts_load = Time.now.to_i
-                        logger.debug("Resetting facter cache after #{cache_time} seconds")
-                        ::Facter.reset
                     end
-                rescue
-                    @@last_facts_load = Time.now.to_i
+
+                    return @@facts
                 end
-    
-                ::Facter.to_hash
+            end
+
+            private
+            def reload_facts
+                ::Facter.reset
+
+                @@facts = {}
+
+                facts = ::Facter.to_hash
+                Log.instance.info("Loaded #{facts.keys.size} facts from Facter")
+
+                facts.each_pair do |key,value|
+                    @@facts[key] = value.to_s
+                end
+
+                # Somehow, sometimes, we get empty fact hashes?
+                if @@facts.empty?
+                    Log.instance.error("Got empty facts, resetting to last known good")
+
+                    @@facts = @last_good_facts.clone
+                else
+                    @@last_good_facts = @@facts.clone
+                end
+
+                @@last_facts_load = Time.now.to_i
             end
         end
     end
